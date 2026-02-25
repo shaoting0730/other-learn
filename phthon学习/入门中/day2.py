eab07caca3218d915b13adfabb31cd2d705cdc41
shaoting0730 <510738319@qq.com>
2026-02-25 19:03:05 +0800

    Update day2.py

--- phthon学习/入门中/day2.py
+++ phthon学习/入门中/day2.py
@@ -2,30 +2,32 @@
 Author: shaoting0730 510738319@qq.com
 Date: 2026-02-24 17:26:57
 LastEditors: shaoting0730 510738319@qq.com
-LastEditTime: 2026-02-25 17:25:00
+LastEditTime: 2026-02-25 17:52:39
 FilePath: /bunny/Users/zhoushaoting/Desktop/GitHub/other-learn/phthon学习/入门中/day2.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
-# 1️⃣ 加载数据
+# 加载数据
 data = load_iris()
 X = data.data
 y = data.target
 
-# 2️⃣ 创建模型
-model = LogisticRegression(max_iter=200)
+# 创建三个不同模型
+model1 = LogisticRegression(max_iter=200)
+model2 = DecisionTreeClassifier()
+model3 = KNeighborsClassifier(n_neighbors=10)
 
-# 3️⃣ 训练模型
-model.fit(X, y)
+# 训练模型
+model1.fit(X, y)
+model2.fit(X, y)
+model3.fit(X, y)
 
-# 4️⃣ 查看准确率
-score = model.score(X, y)
-print("模型准确率:", score)
-
-# 5️⃣ 预测一条数据
-prediction = model.predict([X[0]])
-print("真实结果:", y[0])
-print("预测结果:", prediction[0])
+# 输出准确率
+print("逻辑回归准确率:", model1.score(X, y))
+print("决策树准确率:", model2.score(X, y))
+print("KNN准确率:", model3.score(X, y))
